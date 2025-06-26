98df11213a18864004366508b15cfdf57cadfae9
Olena Tudoi <olena.tudoi@gmail.com>
2025-06-26 09:37:55 +0300

    Update dependencies and refactor product management components to use Ant Design.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import routerProvider, { NavigateToResource } from "@refinedev/react-router";
 
 import { BrowserRouter, Routes, Route, Outlet } from "react-router";
 
+// We'll wrap our app with Ant Design's ConfigProvider to set the theme and App component to use the theme properly.
+import { ConfigProvider, App as AntdApp } from "antd";
+import { ThemedLayoutV2, ThemedTitleV2, useNotificationProvider } from "@refinedev/antd";
+
 import { dataProvider } from "./providers/data-provider";
 import { authProvider } from "./providers/auth-provider";
 
@@ -14,56 +18,69 @@ import { CreateProduct } from "./pages/products/create";
 import { Login } from "./pages/login";
 import { Header } from "./pages/components/header";
 
+// We're importing a reset.css file to reset the default styles of the browser.
+import "antd/dist/reset.css";
+
 export default function App(): JSX.Element {
   return (
     <BrowserRouter>
-    <Refine 
-      dataProvider={dataProvider}
-      authProvider={authProvider}
-      routerProvider={routerProvider}
-      resources={[
-        {
-          name: "protected-products",
-          list: "/products",
-          show: "/products/:id",
-          edit: "/products/:id/edit",
-          create: "/products/create",
-          meta: { label: "Products" },
-        },
-      ]}
-    >
-        <Routes>
-          <Route
-            element={
-              // We're wrapping our routes with the `<Authenticated />` component
-              // We're omitting the `fallback` prop to redirect users to the login page if they are not authenticated.
-              // If the user is authenticated, we'll render the `<Header />` component and the `<Outlet />` component to render the inner routes.
-              <Authenticated key="authenticated-routes" redirectOnFail="/login">
-                <Header />
-                <Outlet />
-              </Authenticated>
-            }
+      <ConfigProvider>
+        <AntdApp>
+          <Refine 
+            dataProvider={dataProvider}
+            authProvider={authProvider}
+            routerProvider={routerProvider}
+            notificationProvider={useNotificationProvider}
+            resources={[
+              {
+                name: "protected-products",
+                list: "/products",
+                show: "/products/:id",
+                edit: "/products/:id/edit",
+                create: "/products/create",
+                meta: { label: "Products" },
+              },
+            ]}
           >
-            <Route index  element={<NavigateToResource resource="protected-products" />} />
-            <Route path="/products">
-              <Route index element={<ListProducts />} />
-              <Route path=":id" element={<ShowProduct />} />
-              <Route path=":id/edit" element={<EditProduct />} />
-              <Route path="create" element={<CreateProduct />} />
-            </Route>
-          </Route>
-          <Route
-            element={
-              <Authenticated key="auth-pages" fallback={<Outlet />}>
-                {/* We're redirecting the user to `/` if they are authenticated and trying to access the `/login` route */}
-                 <NavigateToResource resource="protected-products" />
-              </Authenticated>
-            }
-          >
-            <Route path="/login" element={<Login />} />
-          </Route>
-        </Routes>
-    </Refine>
+              <Routes>
+                <Route
+                  element={
+                    // We're wrapping our routes with the `<Authenticated />` component
+                    // We're omitting the `fallback` prop to redirect users to the login page if they are not authenticated.
+                    // If the user is authenticated, we'll render the `<Header />` component and the `<Outlet />` component to render the inner routes.
+                    <Authenticated key="authenticated-routes" redirectOnFail="/login">
+                       <ThemedLayoutV2
+                        Title={(props) => (
+                        <ThemedTitleV2 {...props} text="Awesome Project" />
+                      )}
+                    >
+                        <Outlet />
+                      </ThemedLayoutV2>
+                    </Authenticated>
+                  }
+                >
+                  <Route index  element={<NavigateToResource resource="protected-products" />} />
+                  <Route path="/products">
+                    <Route index element={<ListProducts />} />
+                    <Route path=":id" element={<ShowProduct />} />
+                    <Route path=":id/edit" element={<EditProduct />} />
+                    <Route path="create" element={<CreateProduct />} />
+                  </Route>
+                </Route>
+                <Route
+                  element={
+                    <Authenticated key="auth-pages" fallback={<Outlet />}>
+                      {/* We're redirecting the user to `/` if they are authenticated and trying to access the `/login` route */}
+                      <NavigateToResource resource="protected-products" />
+                    </Authenticated>
+                  }
+                >
+                  <Route path="/login" element={<Login />} />
+                </Route>
+              </Routes>
+          </Refine>
+        </AntdApp>
+      </ConfigProvider>
     </BrowserRouter>
   );
 }
--- src/pages/products/create.tsx
+++ src/pages/products/create.tsx
@@ -1,55 +1,35 @@
-import { useForm, useSelect } from "@refinedev/core";
+import { useForm, useSelect, Create } from "@refinedev/antd";
+
+import { Form, Input, Select, InputNumber } from "antd";
 
 export const CreateProduct = () => {
-  const { onFinish, mutation } = useForm({
-    // We can also provide `false` to disable the redirect.
-    // Default value is `"list"`.
+  const { formProps, saveButtonProps } = useForm({
     redirect: "edit",
   });
 
-   const { options } = useSelect({
+  const { selectProps } = useSelect({
     resource: "categories",
-    // optionLabel: "title", // Default value is "title" so we don't need to provide it.
-    // optionValue: "id", // Default value is "id" so we don't need to provide it.
   });
 
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    // Using FormData to get the form values and convert it to an object.
-    const data = Object.fromEntries(new FormData(event.currentTarget).entries());
-    // Calling onFinish to submit with the data we've collected from the form.
-    onFinish({
-      ...data,
-      price: Number(data.price).toFixed(2),
-      category: { id: Number(data.category) },
-    });
-  };
-
   return (
-    <form onSubmit={onSubmit}>
-      <label htmlFor="name">Name</label>
-      <input type="text" id="name" name="name" />
-
-      <label htmlFor="description">Description</label>
-      <textarea id="description" name="description" />
-
-      <label htmlFor="price">Price</label>
-      <input type="number" id="price" name="price" step=".01" />
-
-      <label htmlFor="material">Material</label>
-      <input type="text" id="material" name="material" />
-
-      <label htmlFor="category">Category ID</label>
-      <select id="category" name="category">
-        {options?.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select>
-
-      {mutation.isSuccess && <span>successfully submitted!</span>}
-      <button type="submit">Submit</button>
-    </form>
+    <Create saveButtonProps={saveButtonProps}>
+      <Form {...formProps} layout="vertical">
+        <Form.Item label="Name" name="name">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Description" name="description">
+          <Input.TextArea />
+        </Form.Item>
+        <Form.Item label="Material" name="material">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Category" name={["category", "id"]}>
+          <Select {...selectProps} />
+        </Form.Item>
+        <Form.Item label="Price" name="price">
+          <InputNumber step="0.01" stringMode />
+        </Form.Item>
+      </Form>
+    </Create>
   );
 };
